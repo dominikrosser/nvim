0e4f197face4f3a157ae347c90f44e6362c602df
dominikrosser <dominik.rosser@protonmail.ch>
2023-04-11 14:54:42 +0200

    Remap

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -3,6 +3,12 @@ vim.keymap.set("n", "<leader>gg", vim.cmd.Git);
 -- Git status
 vim.keymap.set("n", "<leader>gs", ":G status<CR>");
 
+-- Git fetch
+vim.keymap.set("n", "<leader>gf", ":!git fetch<CR><CR>");
+
+-- Git pull
+vim.keymap.set("n", "<leader>gu", ":!git pull<CR><CR>");
+
 -- Git commit
 vim.keymap.set("n", "<leader>gc", ":G commit<CR>");
 
